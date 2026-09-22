54063bb754a1b801c700c66ab7bf6ce33b569af4
Rafael Ruiz <rafruilop@gmail.com>
2026-09-22 03:55:05 +0200

    fix: add missing useRef import in PinLockModal.js

--- src/components/PinLockModal.js
+++ src/components/PinLockModal.js
@@ -4,7 +4,7 @@
  * Soporta modos: 'unlock', 'setup', 'disable', 'change'.
  */
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
   StyleSheet,
   View,
