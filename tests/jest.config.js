a01ea024b322ab49ceeb5d592062d0b8ab918caa
Rafael Ruiz <rafruilop@gmail.com>
2026-09-26 00:32:09 +0200

    chore: reorganize test configuration files, remove build artifacts, and add project README

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
   preset: 'jest-expo',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  rootDir: path.resolve(__dirname, '..'),
+  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
@@ -10,6 +13,6 @@ module.exports = {
     '!src/constants/**',
   ],
   moduleNameMapper: {
-    '^react-native/setup-env$': '<rootDir>/jest.setup-env.js',
+    '^react-native/setup-env$': '<rootDir>/tests/jest.setup-env.js',
   },
 };
---
+++ tests/jest.setup-env.js
@@ -0,0 +1,1 @@
+// Setup environment for Jest tests
