b61b12e0abf23679085f3ce5ef5d5b8028c095bc
Rafael Ruiz <rafruilop@gmail.com>
2026-09-26 03:09:54 +0200

    chore(release): bump version to 2.0.5 (Build 14)

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -552,7 +552,7 @@ export default function SettingsScreen({ navigation }) {
 
         <View style={styles.versionContainer}>
           <Text variant="caption" style={[styles.versionText, { color: theme.textSecondary }]}>
-            Punteo v2.0.4 (Build 13)
+            Punteo v2.0.5 (Build 14)
           </Text>
         </View>
       </ScrollView>
